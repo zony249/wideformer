10af98c7542d5fd161cf3892b9e928e55a5d8bae
Zony Yu <zony249@gmail.com>
2025-03-03 16:10:38 -0500

    updated training and testing scripts

--- classification/scripts/finetune_teacher.sh
+++ classification/scripts/finetune_teacher.sh
@@ -1,31 +1,33 @@
 #!/bin/bash 
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=a100
+#SBATCH --gpus-per-node=a100:4
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=6
-#SBATCH --time=0-03:00
+#SBATCH --time=7-00:00
 #SBATCH --account=rrg-lilimou
+#SBATCH --output=slurm-logs/slurm-%j-%n-causal-finetune.log
 
 
 
-export CUDA_VISIBLE_DEVICES=6,7
+# export CUDA_VISIBLE_DEVICES=6,7
 export MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
 # export MODEL="roberta-large"
 export TASK_NAME=mnli
-export EXP_NAME=$(date +%x--%T)--TEST
-export OUTPUT=runs/$EXP_NAME
+export EXP_NAME=$(date +%x--%T)--finetune
+export OUTPUT=$SCRATCH/wideformer/classification/runs/$EXP_NAME
 
-mkdir $OUTPUT
+
+mkdir -p $OUTPUT
 
 
 torchrun \
-  --nproc_per_node=2 \
+  --nproc_per_node=4 \
   finetune.py \
     --model_name_or_path $MODEL \
     --is_causal \
     --lora_adapter random_init \
     --task_name $TASK_NAME \
-    --cache_dir=glue \
+    --cache_dir=glue_train \
     --do_train \
     --do_eval \
     --max_seq_length 128 \
@@ -40,4 +42,4 @@ torchrun \
     --bf16_full_eval \
     --optim adamw_hf \
     --seed $((RANDOM % 100000)) \
-  # --overwrite_cache
+    --overwrite_cache
--- classification/scripts/test.sh
+++ classification/scripts/test.sh
@@ -3,28 +3,29 @@
 #SBATCH --gpus-per-node=a100:4
 #SBATCH --mem=64G
 #SBATCH --cpus-per-task=6
-#SBATCH --time=0-03:00
+#SBATCH --time=3-00:00
 #SBATCH --account=rrg-lilimou
 #SBATCH --output=slurm-logs/slurm-%j-%n-causal-pretrained-prediction.out
 
 
-export CUDA_VISIBLE_DEVICES=6,7
+# export CUDA_VISIBLE_DEVICES=6,7
 export MODEL="deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
 # export MODEL="roberta-large"
 export TASK_NAME=mnli
 export EXP_NAME=$(date +%x--%T)--predict
-export OUTPUT=runs/$EXP_NAME
+export OUTPUT=$SCRATCH/wideformer/classification/runs/$EXP_NAME
 
-mkdir $OUTPUT
+
+mkdir -p $OUTPUT
 
 
 torchrun \
-  --nproc_per_node=2 \
+  --nproc_per_node=4 \
   finetune.py \
     --model_name_or_path $MODEL \
     --is_causal \
     --task_name $TASK_NAME \
-    --cache_dir=glue \
+    --cache_dir=glue_pretrained \
     --do_eval \
     --do_predict \
     --max_seq_length 192 \
