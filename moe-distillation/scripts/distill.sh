cef81c95cf5c5420a954dea32e94b604cf886ec3
Zony Yu <zony249@gmail.com>
2025-07-09 16:07:59 -0600

    continue testing on local servers

--- moe-distillation/scripts/distill.sh
+++ moe-distillation/scripts/distill.sh
@@ -6,7 +6,7 @@ nvidia-smi topo -m
 
 
 
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 export HF_HOME=~/large-file-storage
 
 accelerate launch \
@@ -18,8 +18,9 @@ accelerate launch \
         --teacher_model=runs/Qwen3-8B-finetuned-hellaswag \
         --epochs=1 \
         --batch_size=4 \
-        --parallel_lanes=2 \
+        --parallel_lanes=4 \
         --eval_every_steps=500 \
+        --ce_alpha=1.0 --kl_alpha=1.0 --hidden_alpha=1.0 \
         --output_dir=runs/$(date +%Y-%m-%d--%T) \
         --force_load_local_dataset \
         --local_dataset_dir=hellaswag_local \
