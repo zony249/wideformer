dcd0dd666047b3782de670d7e7c948463a43f4d4
Zony Yu <zony249@gmail.com>
2025-07-09 16:06:33 -0600

    merge parallel model

--- moe-distillation/merge_adapters.py
+++ moe-distillation/merge_adapters.py
@@ -2,11 +2,13 @@ import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer 
 from peft import PeftModelForCausalLM
+from models.parallel_models.modeling_qwen3 import Qwen3ForCausalLMParallel
 
-base_model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B", torch_dtype=torch.bfloat16)
+base_model = Qwen3ForCausalLMParallel.from_pretrained("Qwen/Qwen3-0.6B", torch_dtype=torch.bfloat16)
+base_model.parallelize(4)
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
 
-peft_model = PeftModelForCausalLM.from_pretrained(base_model, "runs/Qwen3-0.6B-adapters-hellaswag")
+peft_model = PeftModelForCausalLM.from_pretrained(base_model, "runs/Qwen3-hellaswag-parallel/best_tfmr")
 merged_model = peft_model.merge_and_unload()
-merged_model.save_pretrained("runs/Qwen3-0.6B-finetuned-hellaswag")
-tokenizer.save_pretrained("runs/Qwen3-0.6B-finetuned-hellaswag")
+merged_model.save_pretrained("runs/Qwen3-hellaswag-parallel")
+tokenizer.save_pretrained("runs/Qwen3-hellaswag-parallel")
