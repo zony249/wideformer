75251f6f8c0c950330de25b3d3bdda4ccd451cec
Zony Yu <zony249@gmail.com>
2025-07-09 16:07:37 -0600

    testing qwen parallel loading

--- moe-distillation/test_qwen.py
+++ moe-distillation/test_qwen.py
@@ -1,14 +1,21 @@
-from transformers import AutoTokenizer 
+from transformers import AutoTokenizer, AutoConfig
 from models import Qwen3ForCausalLM, Qwen3ModelParallel, Qwen3ForCausalLMParallel
 
 device=0
 
-model = Qwen3ForCausalLMParallel.from_pretrained("Qwen/Qwen3-0.6B").to(f"cuda:{device}")
-model.parallelize(7)
+config = AutoConfig.from_pretrained("Qwen/Qwen3-0.6B")
+model = Qwen3ForCausalLMParallel(config).to(f"cuda:{device}")
+
+model.parallelize(4)
 tok = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B") 
+
+model.load_from_disk("runs/Qwen3-hellaswag-parallel")
+
+
+
 
 prompt = "Tell me about LLMs please and thank you."
 inputs = tok([prompt], return_tensors="pt").to(f"cuda:{device}")
 
-outputs = model.generate(**inputs, output_hidden_states=True, output_attentions=True, num_beams=5)
-print(tok.batch_decode(outputs[0]))
+outputs = model.generate(**inputs, output_hidden_states=True, output_attentions=True, max_new_tokens=100)
+print("".join(tok.batch_decode(outputs[0])))
