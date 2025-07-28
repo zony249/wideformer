80bc59df24edc7e61f9aa01c35dd4049066237c9
Zony Yu <zony249@gmail.com>
2025-07-28 20:25:15 +0000

    command line arguments

--- moe-distillation/merge_adapters.py
+++ moe-distillation/merge_adapters.py
@@ -4,14 +4,27 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModelForCausalLM
 from models.parallel_models.modeling_qwen3 import Qwen3ForCausalLMParallel
 from model_utils import create_student_from_teacher
+from argparse import ArgumentParser, Namespace 
 
-base = "Qwen/Qwen3-8B"
-adapter = "runs/transfers/q3-8b-hellaswag-all_one-distill/last_tfmr"
-save_as = "runs/transfers/q3-8b-hellaswag-all_one-distill"
+
+parser = ArgumentParser("merge adapters with base model") 
+parser.add_argument("--base_model", required=True, type=str) 
+parser.add_argument("--weight_copy", action="store_true")
+parser.add_argument("--adapter", required=True, type=str)
+parser.add_argument("--save_as", default=None, type=str)
+
+args = parser.parse_args()
+
+base = args.base_model
+adapter = args.adapter
+save_as = "/".join(adapter.split("/")[:-1]) if args.save_as is None else args.save_as
 
 print(f"Saving to {save_as}")
-# base_model = AutoModelForCausalLM.from_pretrained(base, torch_dtype=torch.bfloat16)
-base_model, tok = create_student_from_teacher(base, "weight_copy")
+
+if args.weight_copy:
+    base_model, tok = create_student_from_teacher(base, "weight_copy")
+else: 
+    base_model = AutoModelForCausalLM.from_pretrained(base, torch_dtype=torch.bfloat16)
 # base_model.parallelize(4)
 tokenizer = AutoTokenizer.from_pretrained(base)
 
